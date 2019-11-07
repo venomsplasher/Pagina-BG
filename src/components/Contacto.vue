3d8a3b7a491504854942a927cd6d817964f74606
Juan Ignacio Bidegaray <jbidegaray@uade.edu.ar>
2019-11-07 02:01:10 -0300

    Formato en formulario de contacto

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -4,19 +4,19 @@
         <b-row>
             <b-col>
                 <b-input-group prepend="Nombre y Apellido" class="mt-3">
-                    <b-form-input v-model="this.nombreyapellido"></b-form-input>
+                    <b-form-input type="text" v-model="nombreyapellido"></b-form-input>
                 </b-input-group>
             </b-col>
         </b-row>
         <b-row>
             <b-col>
                 <b-input-group prepend="Telefono" class="mt-3">
-                    <b-form-input v-model="this.telefono"></b-form-input>
+                    <b-form-input v-model="telefono"></b-form-input>
                 </b-input-group>
             </b-col>
             <b-col>
                 <b-input-group prepend="E-mail" class="mt-3">
-                    <b-form-input v-model="this.email"></b-form-input>
+                    <b-form-input v-model="email"></b-form-input>
                 </b-input-group>
             </b-col>
         </b-row>
@@ -26,7 +26,7 @@
                     <b-form-input></b-form-input>
                 </b-input-group> -->
                 <b-form-textarea
-                    v-model="this.consulta"
+                    v-model="consulta"
                     id="textarea-default"
                     placeholder="Consulta"
                     class="mt-3"
@@ -36,19 +36,26 @@
     </div>
 
         <div  id="iconoscontacto" style="width:100%;padding-bottom:5%;padding-top:5%;">
-            <a :href="'mailto:juansolo1988@gmail.com ?subject=Consulta%20Presupuesto%20para%20BG%20Computacion &body=BLABLABLA'+nombreyapellido">
+            <b-row>
+                <b-col cols="4"></b-col>
+                <b-col>
+            <a :href="'mailto:juansolo1988@gmail.com ?subject=Consulta%20Presupuesto%20para%20BG%20Computacion &body=Nombre:%20'+this.nombreyapellido+'%0A'+'Telefono:%20'+this.telefono+'%0A'+'E-Mail:%20'+this.email+'%0A'+'Consulta:%20'+this.consulta+'%0A%0A%0A%0A%0A%0A%0A%0A'">
                 <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src="http://www.suitdoctors.com/wp-content/uploads/2015/11/Email-Icon.png"/>
             </a>
             <div id="consultaemail">
                 <p>Consultanos via Email</p>   
             </div> 
-            <a href="https://wa.me/542254594530?text=Hola!%20este%20es%20un%20mensaje%20automatico%20de%20prueba%20desde%20la%20web%20de%20BGComputacion">
-            <!-- <a href="https://wa.me/542254413556?text=Hola!%20Necesito%20informacion%20sobre..."> -->
+                </b-col>
+                <b-col>
+            <a :href="'https://wa.me/542267531603?text=Nombre:%20'+this.nombreyapellido+'%0A'+'Telefono:%20'+this.telefono+'%0A'+'E-Mail:%20'+this.email+'%0A'+'Consulta:%20'+this.consulta+'%0A%0A'+'Consulta realizada desde BG Computacion Web'">
                 <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src=" http://chaposac.com/wp-content/uploads/2017/09/icono-whatsapp-chaposac.png"/>
             </a>
             <div id="consultawhatsapp" >
                 <p>Consultanos via Whatsapp</p>   
             </div> 
+                </b-col>
+                <b-col cols="4"></b-col>
+            </b-row>
         </div>
         
     </div> 
