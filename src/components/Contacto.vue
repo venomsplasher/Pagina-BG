aea7c1d7b076dd00c5420779ed34f4a2a00a6e74
Juan Ignacio Bidegaray <jbidegaray@uade.edu.ar>
2019-11-07 00:55:11 -0300

    Commit con variables para contacto

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -4,19 +4,19 @@
         <b-row>
             <b-col>
                 <b-input-group prepend="Nombre y Apellido" class="mt-3">
-                    <b-form-input></b-form-input>
+                    <b-form-input v-model="this.nombreyapellido"></b-form-input>
                 </b-input-group>
             </b-col>
         </b-row>
         <b-row>
             <b-col>
                 <b-input-group prepend="Telefono" class="mt-3">
-                    <b-form-input></b-form-input>
+                    <b-form-input v-model="this.telefono"></b-form-input>
                 </b-input-group>
             </b-col>
             <b-col>
                 <b-input-group prepend="E-mail" class="mt-3">
-                    <b-form-input></b-form-input>
+                    <b-form-input v-model="this.email"></b-form-input>
                 </b-input-group>
             </b-col>
         </b-row>
@@ -26,30 +26,29 @@
                     <b-form-input></b-form-input>
                 </b-input-group> -->
                 <b-form-textarea
+                    v-model="this.consulta"
                     id="textarea-default"
                     placeholder="Consulta"
                     class="mt-3"
                 ></b-form-textarea>
             </b-col>
         </b-row>
-        
-        </div>
+    </div>
 
         <div  id="iconoscontacto" style="width:100%;padding-bottom:5%;padding-top:5%;">
-            <a href="mailto:juansolo1988@gmail.com ?subject=Consulta%20Presupuesto%20para%20BG%20Computacion &body=Pasame%20los%20precios%20gato!">
+            <a :href="'mailto:juansolo1988@gmail.com ?subject=Consulta%20Presupuesto%20para%20BG%20Computacion &body=BLABLABLA'+nombreyapellido">
                 <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src="http://www.suitdoctors.com/wp-content/uploads/2015/11/Email-Icon.png"/>
             </a>
-            <div id="consulta" >
+            <div id="consultaemail">
                 <p>Consultanos via Email</p>   
             </div> 
             <a href="https://wa.me/542254594530?text=Hola!%20este%20es%20un%20mensaje%20automatico%20de%20prueba%20desde%20la%20web%20de%20BGComputacion">
             <!-- <a href="https://wa.me/542254413556?text=Hola!%20Necesito%20informacion%20sobre..."> -->
                 <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src=" http://chaposac.com/wp-content/uploads/2017/09/icono-whatsapp-chaposac.png"/>
             </a>
-            <div id="consulta" >
+            <div id="consultawhatsapp" >
                 <p>Consultanos via Whatsapp</p>   
             </div> 
-            
         </div>
         
     </div> 
@@ -62,9 +61,13 @@
 
 export default {
     name: 'Contacto',
-  components: {
-  
-  
-  }
+    data(){
+        return {
+            nombreyapellido:'',
+            telefono:'',
+            email:'',
+            consulta:''
+        }
+    }
 }
 </script>
