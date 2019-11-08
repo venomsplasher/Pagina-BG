563d89bfa230d804b76f0af12184c4c806071056
Juan Ignacio Bidegaray <jbidegaray@uade.edu.ar>
2019-11-08 14:57:55 -0300

    Fix en traerCarrito

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -38,19 +38,26 @@
     </div>
 
         <div  id="iconoscontacto" style="width:100%;padding-bottom:5%;padding-top:5%;">
-            <a :href="'mailto:juansolo1988@gmail.com ?subject=Consulta%20Presupuesto%20para%20BG%20Computacion &body=Nombre:%20'+this.nombreyapellido+'%0A'+'Telefono:%20'+this.telefono+'%0A'+'E-Mail:%20'+this.email+'%0A'+'Consulta:%20'+this.consultaUrl+'%0A%0A%0A%0A%0A%0A%0A%0A'">
-                <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src="http://www.suitdoctors.com/wp-content/uploads/2015/11/Email-Icon.png"/>
-            </a>
-            <div id="consultaemail">
-                <p>Consultanos via Email</p>   
-            </div> 
-                      <a :href="'https://wa.me/542267530998?text=Nombre:%20'+this.nombreyapellido+'%0A'+'Telefono:%20'+this.telefono+'%0A'+'E-Mail:%20'+this.email+'%0A'+'Consulta:%0A'+this.consultaUrl+'%0A%0A'+'Consulta realizada desde BG Computacion Web'">
-            
-                <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src=" http://chaposac.com/wp-content/uploads/2017/09/icono-whatsapp-chaposac.png"/>
-            </a>
-            <div id="consultawhatsapp" >
-                <p>Consultanos via Whatsapp</p>   
-            </div> 
+            <b-row>
+                <b-col cols="4"></b-col>
+                    <b-col>
+                        <a :href="'mailto:juansolo1988@gmail.com ?subject=Consulta%20Presupuesto%20para%20BG%20Computacion &body=Nombre:%20'+this.nombreyapellido+'%0A'+'Telefono:%20'+this.telefono+'%0A'+'E-Mail:%20'+this.email+'%0A'+'Consulta:%20'+this.consultaUrl+'%0A%0A%0A%0A%0A%0A%0A%0A'">
+                            <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src="http://www.suitdoctors.com/wp-content/uploads/2015/11/Email-Icon.png"/>
+                        </a>
+                        <div id="consultaemail">
+                            <p>Consultanos via Email</p>   
+                        </div>
+                    </b-col>
+                    <b-col> 
+                        <a :href="'https://wa.me/542267530998?text=Nombre:%20'+this.nombreyapellido+'%0A'+'Telefono:%20'+this.telefono+'%0A'+'E-Mail:%20'+this.email+'%0A'+'Consulta:%0A'+this.consultaUrl+'%0A%0A'+'Consulta realizada desde BG Computacion Web'">
+                            <img class="imagen" style="display:block; margin:auto; padding-top:0%; width:100px"  src=" http://chaposac.com/wp-content/uploads/2017/09/icono-whatsapp-chaposac.png"/>
+                        </a>
+                        <div id="consultawhatsapp" >
+                            <p>Consultanos via Whatsapp</p>   
+                        </div> 
+                    </b-col>
+                <b-col cols="4"></b-col>
+            </b-row>
         </div>
         <!-- 542254594530 -->
     </div> 
@@ -79,12 +86,10 @@ export default {
         traerCarrito(){
             for(var i in this.$store.getters.getCarrito){
             this.consulta += "Producto: "+this.$store.getters.getCarrito[i].titulo+"\nMarca: "+this.$store.getters.getCarrito[i].marca+"\nCantidad: "+this.$store.getters.getCarrito[i].cantidad+"\n\n"
+            this.consultaUrl += "%0AProducto: "+this.$store.getters.getCarrito[i].titulo+"%0AMarca: "+this.$store.getters.getCarrito[i].marca+"%0ACantidad: "+this.$store.getters.getCarrito[i].cantidad+"%0A%0A"
             }
             //armo la string para que quede bien en la url 
 
-            for(var i in this.$store.getters.getCarrito){
-                this.consultaUrl += "%0AProducto: "+this.$store.getters.getCarrito[i].titulo+"%0AMarca: "+this.$store.getters.getCarrito[i].marca+"%0ACantidad: "+this.$store.getters.getCarrito[i].cantidad+"%0A%0A"
-            }
         }
     },
     mounted(){
